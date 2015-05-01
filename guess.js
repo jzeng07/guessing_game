047397bc1440a4762a4d2f2eeb9a4db23252d965
jzeng07 <jzeng.zeng.01@gmail.com>
2015-05-01 14:35:11 -0700

    Change the file guess.js

--- guess.js
+++ guess.js
@@ -1,3 +1,5 @@
 var userChoice = prompt("Please guess: Where am I from? Americas, Europe or Asia ");
 var myChoice = "Asia";
 document.write("A ha")
+
+document.write("with git to control the version")
